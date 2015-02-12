f6da31aadc1318139005d8f527e904af2048650f
Brian Leonard <brian@theleoreport.com>
2015-02-12 15:41:46 -0500

    product listing _.template created

---
+++ main.js
@@ -0,0 +1,18 @@
+var productListingTemplate = _.template(
+  "<div class='product'>" +
+    "<div class='product-wrapper'>" +
+      "<div class='product-image-wrapper'>" +
+        "<div class='product-image'>" +
+          "<img src='<%= Images[0].url_fullxfull %>'>" +
+        "</div>" +
+      "</div>" +
+      "<div class='product-info'>" +
+        "<div class='product-title'><a href='<%= url %>'><%= title %></a></div>" +
+        "<div class='product-details'>" +
+          "<div class='product-seller'><a href='<% Shop.url %>'><%= Shop.shop_name %></a></div>" +
+          "<div class='product-price'>$<%= price %> <%= currency_code %></div>" +
+        "</div>" +
+      "</div>" +
+    "</div>"+
+  "</div>"
+);
